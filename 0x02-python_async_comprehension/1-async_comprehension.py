356b7520cc12f72819bc2b05646520a03f0b507d
Uthman Durosinlohun <udurosinlohun@gmail.com>
2023-07-11 10:25:25 -0700

    fixed bug

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -6,5 +6,5 @@ from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> List[float]:
+async def async_comprehension() -> Generator[float, None, None]:
     return [values async for values in async_generator()]
