9444bd1a0fcb2060b73b08e3ef854f96dd930599
Uthman Durosinlohun <udurosinlohun@gmail.com>
2023-07-11 10:19:50 -0700

    fixed return type in task 0

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,10 +2,10 @@
 """writes a coroutine called async_generator"""
 import asyncio
 import random
-from typing import List
+from typing import Generator
 
 
-async def async_generator() -> List[float]:
+async def async_generator() -> Generator[float, None, None]:
     """yields a random number between 0 and 10"""
     for i in range(10):
         await asyncio.sleep(1)
