ae391c47926b656dcb6895cb590a1ff1ad11dd47
Uthman Durosinlohun <udurosinlohun@gmail.com>
2023-07-11 10:52:21 -0700

    fiexed return type

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -2,9 +2,9 @@
 """async comperhensions"""
 import asyncio
 import random
-from typing import Generator, List
+from typing import Iterator
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> List[float]:
+async def async_comprehension() -> Iterator[float]:
     return [values async for values in async_generator()]
--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -3,7 +3,7 @@
 import asyncio
 import random
 import time
-from typing import Generator, List
+from typing import Iterator
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
